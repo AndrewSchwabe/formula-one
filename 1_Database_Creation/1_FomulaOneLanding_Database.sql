96f017233953ecd99e4b21ea5e92868399103eb1
andrewschwabe <schwabea530@gmail.com>
2022-03-04 22:02:25 -0600

    Resovle issue with not closing all connections before dropping DB

--- 1_Database_Creation/1_FomulaOneLanding_Database.sql
+++ 1_Database_Creation/1_FomulaOneLanding_Database.sql
@@ -1,6 +1,8 @@
 if db_id('FormulaOneLanding') is not null
+	use master
 	alter database FormulaOneLanding set single_user with rollback immediate
 	drop database FormulaOneLanding
 go
 
 create database FormulaOneLanding
+go
