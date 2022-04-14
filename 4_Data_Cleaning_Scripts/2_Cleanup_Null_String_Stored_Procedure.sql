dbb10d15a0f974ceb0f29cd550a5d398dd00a11d
andrewschwabe <schwabea530@gmail.com>
2022-04-13 22:07:29 -0500

    Analysis data cleaned

--- 4_Data_Cleaning_Scripts/2_Cleanup_Null_String_Stored_Procedure.sql
+++ 4_Data_Cleaning_Scripts/2_Cleanup_Null_String_Stored_Procedure.sql
@@ -1,14 +1,14 @@
 use FormulaOneAnalysis
 
-drop procedure if exists dbo.RemoveDoubleQuotes  
+drop procedure if exists dbo.NullCleanup  
 go
 
-create procedure dbo.RemoveDoubleQuotes 
+create procedure dbo.NullCleanup 
 	@tableName varchar(50)
 as
 	declare @sqlStatement varchar(max)
 
-	select @sqlStatement = coalesce(@sqlStatement + ', ', '') + concat(column_name, ' = replace(', column_name, ', ''"'', '''')')
+	select @sqlStatement = coalesce(@sqlStatement + ', ', '') + concat(column_name, ' =  case when ', column_name, ' = ''\N'' then null else ', column_name, ' end')
 	from INFORMATION_SCHEMA.COLUMNS
 	where table_name = @tableName and data_type = 'varchar'
 
